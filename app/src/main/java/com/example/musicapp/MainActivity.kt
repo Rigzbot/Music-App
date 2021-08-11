9ecf8c6ac73d4d45a28063e58094d88abff46c98
Rishik Gupta <rishikgupta8@gmail.com>
2021-08-11 19:15:18 +0530

    Implemented Song Fragment and color changing background

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.musicapp
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.musicapp.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,5 +25,15 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
+
+        //up button navigates to songView when active
+        binding.arrowUpBottomBar.setOnClickListener {
+            //to clear previous navigation history
+            navController.navigate(R.id.songFragment)
+
+            //close menu and nav bar after going to music screen
+            binding.currentSongBottomBar.visibility = View.GONE
+            binding.navView.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/example/musicapp/ui/song/SongFragment.kt
+++ app/src/main/java/com/example/musicapp/ui/song/SongFragment.kt
@@ -1,17 +1,22 @@
 package com.example.musicapp.ui.song
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.palette.graphics.Palette
 import com.example.musicapp.R
-import com.example.musicapp.databinding.FragmentSearchBinding
 import com.example.musicapp.databinding.SongFragmentBinding
 import com.example.musicapp.ui.search.SearchViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SongFragment : Fragment() {
 
@@ -32,12 +37,60 @@ class SongFragment : Fragment() {
 
         _binding = SongFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //palette api to set background color
+        val drawable = binding.songImage.drawable
+        val bitmap = drawable.toBitmap()
+        setBackgroundColor(bitmap)
+
+        //back button action
+        binding.backButtonSongFragment.setOnClickListener {
+            findNavController().navigateUp() //remove last stack
+        }
+    }
+
+    //change background color of song view
+    /**
+     * (TO DO: check if dominant color already present in database, if not then look for it
+     * and add in database)
+     */
+    private fun setBackgroundColor(bitmap: Bitmap) {
+        Palette.Builder(bitmap).generate {
+            it?.let { palette ->
+                //getting vibrant color
+                val dominantColor = palette.getVibrantColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.darkGray
+                    )
+                )
+                val hexColor = "#" + Integer.toHexString(dominantColor).substring(2)
+
+                //creating gradient background
+                val gradientDrawable = GradientDrawable(
+                    GradientDrawable.Orientation.TOP_BOTTOM,
+                    intArrayOf(
+                        Color.parseColor(hexColor),
+                        Color.parseColor("#000000")
+                    )
+                )
+                gradientDrawable.cornerRadius = 0f
+                //Set Gradient
+                binding.songFragmentLayout.background = gradientDrawable
+            }
+        }
+    }
+
+    //brings back nav bar and current song bottom bar when exiting song fragment
     override fun onDestroyView() {
         super.onDestroyView()
+        requireActivity().nav_view.visibility = View.VISIBLE
+        requireActivity().currentSongBottomBar.visibility = View.VISIBLE
         _binding = null
     }
 }
